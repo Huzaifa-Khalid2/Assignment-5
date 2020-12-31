60b646f21df8c506186ca667d654bb9c0061366f
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2020-12-31 23:40:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let d = 'd = five centimeters'
+side1 = 'd = 5cm'
+alert(d)
+let formula = 'a = π x d'
+formula = 'a = π x d'
+alert(formula)
+let equation = 'a = π x 5'
+equation = 'a = π x 5'
+alert(equation)
+let area = 'a = π x 5'
+area = 'a = 15.7cm'
+alert(area)
+let statement = 'Therefore the area of this circle is 15.7cm'
+statement = 'Therefore the area of this circle is 15.7cm'
+alert(statement)
